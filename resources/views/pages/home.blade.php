90575d9e8f53ff8cef823882bac8ba3168f0197d
faturrahman04 <faturrahmanalfarisi449@gmail.com>
2025-09-19 22:32:47 +0700

    feat(content): Menambah kontent pada section page untuk dashboard

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -2,6 +2,26 @@
 
 @section('Crudy | Home')
 
-@section('aside')
-  <x-sidebar></x-sidebar>
+@section('page')
+  <div class="p-8">
+    <h1 class="font-medium text-3xl font-inter">Dashboard</h1>
+
+    <div class="py-4">
+      <ul class="grid grid-cols-2 gap-4">
+
+      <x-card-info 
+        number="200"
+        bgImage="bg-blue-500/30"
+      >Jumlah pengguna
+      </x-card-info>
+
+      <x-card-info
+        number="200"
+        bgImage="bg-orange-500/30"
+        image="./assets/books.svg"
+      >Jumlah buku</x-card-info>
+
+      </ul>
+    </div>
+  </div>
 @endsection
